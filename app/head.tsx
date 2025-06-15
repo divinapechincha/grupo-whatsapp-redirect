adf3d017f5dba32c81ea62ee1a1a2ced0211f0f6
divinapechincha <divinapechincha2@gmail.com>
2025-06-14 22:21:56 -0300

    Configura idioma, head e atualiza título

---
+++ app/head.tsx
@@ -0,0 +1,10 @@
+// app/head.tsx
+export default function Head() {
+  return (
+    <>
+      <title>Divina Pechincha</title>
+      <meta name="description" content="Ofertas VIP com os melhores preços da internet na Divina Pechincha!" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+    </>
+  );
+}
