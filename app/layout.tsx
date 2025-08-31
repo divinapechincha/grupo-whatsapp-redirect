256be0b8c51bf1453a4e1ce7cb650d5a3a49f10e
divinapechincha <divinapechincha2@gmail.com>
2025-08-30 22:12:59 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,17 +26,16 @@ export default function RootLayout({
   return (
     <html lang="pt-br">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        {/* Google Analytics 4 */}
-        <Script
-          src="https://www.googletagmanager.com/gtag/js?id=G-3Z83EGB4VK"
-          strategy="afterInteractive"
-        />
-        <Script id="google-analytics" strategy="afterInteractive">
+        {/* Google Tag Manager */}
+        <Script id="gtm-head" strategy="afterInteractive">
           {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-3Z83EGB4VK');
+            (function(w,d,s,l,i){w[l]=w[l]||[];
+            w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
+            var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+            j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+            f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-XXXXXXX');
           `}
         </Script>
 
@@ -64,6 +63,7 @@ export default function RootLayout({
             alt=""
           />
         </noscript>
+
         {children}
       </body>
     </html>
